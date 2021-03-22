77d68069dc5f81b4c3bcb23a7d966b06fbed33eb
NathanRobinson11 <77732303+NathanRobinson11@users.noreply.github.com>
2021-03-22 10:47:15 +0000

    Update Smartwatches.js

--- client/src/components/categories/smartwatches/Smartwatches.js
+++ client/src/components/categories/smartwatches/Smartwatches.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Smartwatches.css'
-import Footer from '../common/footer/Footer'
 
 const Smartwatches = () => {
     return(
-        <Footer/>
+        <>
+        </>
     )
 }
 

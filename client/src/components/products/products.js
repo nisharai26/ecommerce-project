c5f5840ba0714a1234e159969a89a6791ccf7e44
NathanRobinson11 <77732303+NathanRobinson11@users.noreply.github.com>
2021-03-22 10:49:40 +0000

    Update products.js

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Products.css'
-import Footer from '../common/footer/Footer'
 
 const Products = () => {
     return(
-        <Footer/>
+        <>
+        </>
     )
 }
 

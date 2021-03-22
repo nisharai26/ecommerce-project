531702786d7ff2d50f930372c7604139e1aa8709
NathanRobinson11 <77732303+NathanRobinson11@users.noreply.github.com>
2021-03-22 10:46:06 +0000

    Update Desktops.js

--- client/src/components/categories/desktops/Desktops.js
+++ client/src/components/categories/desktops/Desktops.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Desktops.css'
-import Footer from '../common/footer/Footer'
 
 const Desktops = () => {
     return(
-        <Footer/>
+        <>
+        </>
     )
 }
 

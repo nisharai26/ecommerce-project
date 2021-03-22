9186556a608e9c7b44d661466afcb9007478962f
NathanRobinson11 <77732303+NathanRobinson11@users.noreply.github.com>
2021-03-22 10:50:07 +0000

    Update sign-in-up.js

--- client/src/components/sign-in-up/sign-in-up.js
+++ client/src/components/sign-in-up/sign-in-up.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Sign-in-up.css'
-import Footer from '../common/footer/Footer'
 
 const SignInUp = () => {
     return(
-        <Footer/>
+        <>
+        </>
     )
 }
 

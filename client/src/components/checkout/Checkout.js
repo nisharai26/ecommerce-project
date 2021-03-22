70e8258c8292edcfb6123485531e7a38d6bb7f60
NathanRobinson11 <77732303+NathanRobinson11@users.noreply.github.com>
2021-03-22 10:47:42 +0000

    Update Checkout.js

--- client/src/components/checkout/Checkout.js
+++ client/src/components/checkout/Checkout.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Checkout.css'
-import Footer from '../common/footer/Footer'
 
 const Checkout = () => {
     return(
-        <Footer/>
+        <>
+        </>
     )
 }
 

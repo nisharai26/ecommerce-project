71cfee8ffd7836060932c2a68d91829288547d39
NathanRobinson11 <77732303+NathanRobinson11@users.noreply.github.com>
2021-03-22 10:45:11 +0000

    Update Basket.js

--- client/src/components/basket/Basket.js
+++ client/src/components/basket/Basket.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Basket.css'
-import Footer from '../common/footer/Footer'
 
 const Basket = () => {
     return(
-        <Footer/>
+        <>
+        </>
     )
 }
 

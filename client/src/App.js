f7df2dd1629267ba8a706c9b5b75c45fe3118bfe
Matt <m.connell97@hotmail.co.uk>
2021-03-22 11:42:30 +0000

    Installed react router and imported in App.js
    Please 'npm i' to install locally

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 // import Navbar from './components/common/navbar/Navbar';
 import Home from './components/home/Home';
 // import Navbar from './components/common/product-card/Product-card';

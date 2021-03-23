9153d5fe031a7ec12794aa5f6917c6d1be979d29
Nathan Robinson <Nathan11Robinson@gmail.com>
2021-03-23 17:38:34 +0000

    Sign In page front end complete

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -2,17 +2,14 @@ import React from 'react'
 import './Home.css'
 import CategoryRow from './CategoryRow'
 
-
 const Home = () => {
     return(
         <div className ='main-home'>
-
             <CategoryRow categoryTitle = 'Laptops'/>
             <CategoryRow categoryTitle = 'Desktops'/>
             <CategoryRow categoryTitle = 'Mobiles'/>
             <CategoryRow categoryTitle = 'Smart Watches'/>
             <CategoryRow categoryTitle = 'Accessories'/>
-
         </div>
     )
 }

b559d950b57c114720219931b7da693a2472e107
Matt <m.connell97@hotmail.co.uk>
2021-03-24 14:29:19 +0000

    Updated category row and product card components

--- client/src/components/common/product-card/Product-card.js
+++ client/src/components/common/product-card/Product-card.js
@@ -8,17 +8,24 @@ let productImages = [
 
 const ProductCard = (props) => {
     return(
+        <Link>
         <div className="product-card">
-            <div className="image-container">
-                <img src={props.productImage} alt=""/>
-                <p>{props.price}</p>
-            </div>
-            <div className="product-info">
-                <h3>{props.productTitle}</h3>
-                <p>{props.description}</p>
-                <span>Add to Basket</span>
+            <img src="https://brain-images-ssl.cdn.dixons.com/4/0/10216304/u_10216304.jpg" alt="Macbook Pro 13.3 (2020)"/>
+            <div className="info-container">
+            <h3>APPLE MacBook Pro 13.3" (2020) - M1, 256 GB SSD, Space Grey</h3>
+            <p>£1152.00</p>
+            <div className="expand-background">
+                    <ul>
+                        <li>macOS 11.0 Big Sur</li>
+                        <li>Apple M1 chip</li>
+                        <li>RAM: 8 GB / Storage: 256 GB SSD</li>
+                        <li>Retina display</li>
+                        <li>Battery life: Up to 20 hours</li>
+                    </ul>
+                </div>
             </div>
         </div>
+        </Link>
     )
 }
 
--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import './Home.css'
-import CategoryRow from './CategoryRow'
-
+import React from 'react';
+import './Home.css';
+import CategoryRow from '../common/categoryRow/CategoryRow';
 
 const Home = () => {
     return(
         <div className ='main-home'>
 
-            <CategoryRow categoryTitle = 'Laptops'/>
-            <CategoryRow categoryTitle = 'Desktops'/>
-            <CategoryRow categoryTitle = 'Mobiles'/>
-            <CategoryRow categoryTitle = 'Smart Watches'/>
-            <CategoryRow categoryTitle = 'Accessories'/>
+            <CategoryRow categoryTitle = 'Laptops' pageLink = "/categories/laptops"/>
+            <CategoryRow categoryTitle = 'Desktops' pageLink = "/categories/desktops"/>
+            <CategoryRow categoryTitle = 'Mobiles' pageLink = "/categories/mobiles"/>
+            <CategoryRow categoryTitle = 'Smart Watches' pageLink = "/categories/smartwatches"/>
+            <CategoryRow categoryTitle = 'Accessories' pageLink = "/categories/accessories"/>
 
         </div>
     )

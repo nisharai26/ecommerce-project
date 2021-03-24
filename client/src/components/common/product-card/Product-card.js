5c9fc9f8b485c59805d84b45a3327437f6e337d2
Matt <m.connell97@hotmail.co.uk>
2021-03-24 21:38:10 +0000

    Fixed product card info on-hover issue

--- client/src/components/common/product-card/Product-card.js
+++ client/src/components/common/product-card/Product-card.js
@@ -9,22 +9,26 @@ let productImages = [
 const ProductCard = (props) => {
     return(
         <Link>
-        <div className="product-card">
-            <img src="https://brain-images-ssl.cdn.dixons.com/4/0/10216304/u_10216304.jpg" alt="Macbook Pro 13.3 (2020)"/>
-            <div className="info-container">
-            <h3>APPLE MacBook Pro 13.3" (2020) - M1, 256 GB SSD, Space Grey</h3>
-            <p>£1152.00</p>
-            <div className="expand-background">
-                    <ul>
-                        <li>macOS 11.0 Big Sur</li>
-                        <li>Apple M1 chip</li>
-                        <li>RAM: 8 GB / Storage: 256 GB SSD</li>
-                        <li>Retina display</li>
-                        <li>Battery life: Up to 20 hours</li>
-                    </ul>
+            <div className="product-card">
+                <div className = "product-info">
+                    <img src="https://brain-images-ssl.cdn.dixons.com/4/0/10216304/u_10216304.jpg" alt="Macbook Pro 13.3 (2020)"/>
+                    <div className="info-container">
+                        <h3>APPLE MacBook Pro 13.3" (2020) - M1, 256 GB SSD, Space Grey</h3>
+                        <p>£1152.00</p>
+                    </div>
+                </div>
+                <div className="expanded-background-container">
+                    <div class="expand-background">
+                        <ul>
+                            <li>macOS 11.0 Big Sur</li>
+                            <li>Apple M1 chip</li>
+                            <li>RAM: 8 GB / Storage: 256 GB SSD</li>
+                            <li>Retina display</li>
+                            <li>Battery life: Up to 20 hours</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
         </Link>
     )
 }

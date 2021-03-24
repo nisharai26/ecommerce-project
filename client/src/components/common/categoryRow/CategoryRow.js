2e9774787ecb0d1a238ce002201c48b29d29c1ae
Matt <m.connell97@hotmail.co.uk>
2021-03-24 15:27:17 +0000

    Updated category row component
    Removed category headings for use on other pages

--- client/src/components/common/categoryRow/CategoryRow.js
+++ client/src/components/common/categoryRow/CategoryRow.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './CategoryRow.css';
-import {Link} from 'react-router-dom';
 import ProductCard from '../product-card/Product-card';
 
 const CategoryRow = (props) => {
     return (
 
         <div className ='category content'>
-
-            <Link to = {props.pageLink}><h2>{props.categoryTitle}</h2></Link>
             <div className = 'category-row'>
                 <ProductCard/>
                 <ProductCard/>

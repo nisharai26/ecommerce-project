41558a23f3e4b40881bfb32b3fb7f2bfb3860c3c
Matt <m.connell97@hotmail.co.uk>
2021-03-29 12:10:32 +0100

    Updated product schema

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -2,12 +2,14 @@ const {Schema,model}= require('mongoose');
 
 
 const product = new Schema({
-    title: {type: String, required: true},
     category: {type: String, required: true},
-    manufacturer: {type: String, required: true},
+    image: {type: String, required: true},
+    imageAlt: {type: String, required: true},
+    brand: {type: String, required: true},
+    model: {type: String, required: true},
     price: {type: Number, required: true},
     description: {type: String, required: true}
 })
 
 
-module.exports=model('products', product);
+module.exports=model('inventory', product);

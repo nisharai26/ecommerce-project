4d21216a758edc56ce2118f2fb7570d817a9e775
Matt <m.connell97@hotmail.co.uk>
2021-03-29 11:43:05 +0100

    Removed comments

--- client/src/components/common/categoryRow/CategoryRow.js
+++ client/src/components/common/categoryRow/CategoryRow.js
@@ -8,18 +8,14 @@ const CategoryRow = () => {
     return (
         <div className ='category content'>
             <div className = 'category-row'>
-                {newProducts.map((product, index) => { {/* newProducts.map = loops through newProducts array and gives access to each object individually */}
+                {newProducts.map((product, index) => {
                     return (
                         <ProductCard product={product}/>
                     )
                 })}
-                {/* Should return <ProductCard/>s for the first 3 items in the array for this category, pulling the category,
-                 image, imageAlt, brand, model, price, and description from ProductData.js, which should be populated with info from the database. */}
             </div>
         </div>
     )
 }
-
-// console.log(data.products.slice(0,3));
 
 export default CategoryRow;

059abfff768fa725b69704cfb42185dcdf6bad82
Matt <m.connell97@hotmail.co.uk>
2021-03-31 15:08:44 +0100

    Added "Add to Basket" button to product cards

--- client/src/components/common/product-card/Product-card.js
+++ client/src/components/common/product-card/Product-card.js
@@ -2,12 +2,33 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './Product-card.css';
 import data from '../../../ProductData';
+import {useState, useEffect} from 'react';
 
 let productImages = [
 
 ]
 
 const ProductCard = (props) => {
+    // console.log(`${props.product._id}`);
+    const [product, basket] = useState([])
+
+    const addToBasket= (product) => {
+        console.log(product);
+        let basket = localStorage.setItem('products', JSON.parse(product));
+
+        // if (basket) {
+        //     basket = JSON.parse(basket);
+        //     if (basket.hasOwnProperty(product)) {
+        //         basket[product].quantity++
+        //     }
+        //     else {
+        //         basket[product] = {
+        //             _id: product,
+        //             quantity: 1
+        //         }
+        //     }
+        // }
+    }
     return(
         <Link>
         {/* {data.products.map(product =>  */}
@@ -17,6 +38,7 @@ const ProductCard = (props) => {
                 <div className="info-container">
                     <h3>{props.product.brand} {props.product.model}</h3>
                     <p>£{props.product.price}</p>
+                    <button onclick={() => {addToBasket(props.product._id)}}>Add to Basket</button>
                 </div>
             </div>
             <div className="expanded-background-container">
